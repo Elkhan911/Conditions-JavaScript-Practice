4ce49b2b62f647b50ed205424a01d11bf2748b24
Elkhan911 <955pro@mail.ru>
2022-11-05 15:50:42 +0400

    tasks 1-2

---
+++ script.js
@@ -0,0 +1,36 @@
+// Условия в JavaScript
+
+/* 1.  В переменной day лежит какое-то число из интервала от 1 до 31. 
+Определите в какую декаду месяца попадает это число 
+(в первую, вторую или третью).*/
+
+let day1 = 27;
+
+if (day1 < 10) {
+  console.log("pervaya dekada");
+} else if (day1 > 10 && day1 < 20) {
+  console.log("vtoraya dekada");
+} else if (day1 > 20 && day1 < 30) {
+  console.log("tretya dekada");
+} else {
+  console.log("vi vveli nevernoe znachenie");
+}
+
+/* 2. Пусть в переменной num хранится число. Если это число попадает 
+в диапазон от 10 до 99, то найдите сумму цифр этого числа. 
+Если полученная сумма меньше или равна 9, 
+то выведите сообщение о том, что сумма цифр однозначна, 
+в противном случае выведите сообщение о том, что сумма цифр двухзначна. */
+
+let num2 = 14;
+
+if (num2 >= 10 && num2 <= 99) {
+  let sum = Number(String(num2)[0]) + Number(String(num2)[1]);
+  if (sum <= 9) {
+    console.log("сумма цифр однозначна");
+  } else {
+    console.log("сумма цифр двухзначна");
+  }
+} else {
+  console.log("Chislo ne popadayet ot 10 do 99");
+}
