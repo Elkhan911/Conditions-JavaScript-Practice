35bf7d72e5f873b3c1b2b71d8b4896e81c5718d3
Elkhan911 <955pro@mail.ru>
2022-11-05 15:57:44 +0400

    task 3

--- script.js
+++ script.js
@@ -34,3 +34,35 @@ if (num2 >= 10 && num2 <= 99) {
 } else {
   console.log("Chislo ne popadayet ot 10 do 99");
 }
+
+/* 3. Перепишите следующий код через switch-case:
+let lang = 'ru';
+
+if (lang == 'ru') {
+	console.log('рус');
+} else if (lang == 'en') {
+	console.log('анг');
+} else if (lang == 'de') {
+	console.log('нем');
+} else {
+	console.log('язык не поддерживается');
+} */
+
+let lang = "ru";
+
+switch (lang) {
+  case "ru":
+    console.log("rus");
+    break;
+
+  case "en":
+    console.log("анг");
+    break;
+
+  case "de":
+    console.log("нем");
+    break;
+
+  default:
+    console.log("язык не поддерживается");
+}
