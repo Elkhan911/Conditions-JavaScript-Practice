b39e62e3f11847003b619878d6ac2cdc0d94a996
Elkhan911 <955pro@mail.ru>
2022-11-16 00:54:18 +0400

    tas 4

--- script.js
+++ script.js
@@ -66,3 +66,19 @@ switch (lang) {
   default:
     console.log("язык не поддерживается");
 }
+
+/* 4. Перепишите следующий код через тернарный оператор:
+let num = 1;
+let res;
+
+if (num >= 0) {
+	res = '1';
+} else {
+	res = '2';
+}
+
+console.log(res); */
+
+let num4 = 1;
+let res4 = num4 >= 0 ? "1" : "2";
+console.log(res4);
